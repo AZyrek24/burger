a461f5529da7a03c07724e8239e8f37f7e26e066
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-30 16:31:22 -0700

    initial files, folders, layouts, gitignore

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,12 @@
+var express = require('express');
+// Import the model (burgers.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,17 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burgers = {
+  selectAll: function() {
+    
+  },
+  insertOne: function() {
+    
+  },
+  updateOne: function() {
+    
+  },
+};
+
+// Export the database functions for the controller
+module.exports = burgers;
